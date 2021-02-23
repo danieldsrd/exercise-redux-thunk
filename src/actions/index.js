16ebda86b3aeb7586a59e90bd1b3f25f2085d4c1
Daniel <danieldsrd@gmail.com>
2021-02-23 15:15:23 -0300

    Aplicacao de API

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,5 +6,11 @@ export const requestAPI = () => ({ type: REQUEST_API });
 export const getPicture = (data) => ({ type: GET_PICTURE, data });
 
 export function fetchAPI() {
-  // Desenvolva aqui o código da action assíncrona
+  return async (dispatch) => {    
+    dispatch(requestAPI());
+    const apiPicture = await fetch('https://aws.random.cat/meow')
+    const catJson = await apiPicture.json();
+
+    return dispatch(getPicture(catJson));
+  }
 }
