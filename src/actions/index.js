aa8380926a35cdda6e6ccd07742d4f0a1e2b5b0f
Daniel <danieldsrd@gmail.com>
2021-02-23 19:05:53 -0300

    mudando api

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,9 +8,9 @@ export const getPicture = (data) => ({ type: GET_PICTURE, data });
 export function fetchAPI() {
   return async (dispatch) => {    
     dispatch(requestAPI());
-    const apiPicture = await fetch('https://aws.random.cat/meow')
+    const apiPicture = await fetch('https://api.thecatapi.com/v1/images/search')
     const catJson = await apiPicture.json();
 
-    return dispatch(getPicture(catJson));
+    return dispatch(getPicture(catJson[0].url));
   }
 }
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,7 +5,7 @@ import defaultCat from '../images/detCat.gif';
 
 class Gallery extends Component {
   render() {
-    const { imgPath, isLoading, useDefaultImg } = this.props;
+    const { imgPath, isLoading, useDefaultImg } = this.props;    
     return (
       <div className="gallery-container">
         { isLoading && <div>LOADING...</div> }
@@ -23,7 +23,7 @@ Gallery.propTypes = {
 };
 
 const mapStateToProps = (state) => ({
-  imgPath: state.gallery.imgURL.file,
+  imgPath: state.gallery.imgURL,
   isLoading: state.gallery.isLoading,
   useDefaultImg: state.gallery.defaultImg,
 });
